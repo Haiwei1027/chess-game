bbce9879bf25e75748f149446302f836f89235e3
Haiwei He <Haiwei.he@outlook.com>
2022-11-03 13:30:36 +0000

    refactor board class

--- src/main/java/me/haiwei/ChessBoard.java
+++ src/main/java/me/haiwei/ChessBoard.java
@@ -7,13 +7,16 @@ import me.haiwei.pieces.*;
 
 public class ChessBoard { // pawn, knight, rook, bishop, king, queen //white, black
 
-	public final int PAWN=0,KNIGHT=1,ROOK=2,BISHOP=3,KING=4,QUEEN=5;
+	public final static int PAWN=0,KNIGHT=1,ROOK=2,BISHOP=3,KING=4,QUEEN=5;
 	public final int WHITE=0,BLACK=1;
+
+	private final int size = 8;
+
 	public BufferedImage image;
 
 	private ChessPiece[] pieceTypes;
-	private int[][] board;
-	private int row, col;
+
+	private ChessPiece[][] board;
 
 	private Main main;
 	private Point selected;
@@ -26,16 +29,10 @@ public class ChessBoard { // pawn, knight, rook, bishop, king, queen //white, bl
 	private int whiteCastleDir = 0, blackCastleDir = 0;
 	private Color checkColor = new Color(255,0,0,100);
 
-	public ChessBoard(Main main,int row,int col) {
+	public ChessBoard(Main main) {
 		this.main = main;
-		this.row = row;
-		this.col = col;
-		board = new int[row][col];
-		for (int x = 0; x < row; x++) {
-			for (int y = 0; y < col; y++) {
-				board[x][y] = -1;
-			}
-		}
+		board = new ChessPiece[getSize()][getSize()];
+
 		Bishop bishop = new Bishop(this);
 		Rook rook = new Rook(this);
 		pieceTypes = new ChessPiece[] { new Pawn(this), new Knight(this), rook, bishop,
@@ -45,27 +42,18 @@ public class ChessBoard { // pawn, knight, rook, bishop, king, queen //white, bl
 		paint();
 	}
 
-	public boolean canWhiteCastle(int dir){
-		return whiteCastleDir == dir || whiteCastleDir == 0;
-	}
-
-	public boolean canBlackCastle(int dir){
-		return blackCastleDir == dir || blackCastleDir == 0;
-	}
-
-	public int getEnPassantFile(){
-		return enPassantFile;
+	public int getSize(){
+		return size;
 	}
 
 	public void resetBoard(){
 		for (int i = 0; i < 8; i++) {
-			board[i][6] = PAWN;
-			board[i][1] = PAWN+6;
-			// pieces[i][1] = i+6;
+			board[i][6] = new Pawn(board,i,6,side);
+			board[i][1] = new Pawn(board,i,1,side);
 		}
 		for (int i = 0; i < 2; i++){
 			for (int j = 0; j < 2; j++) {
-				board[j*7][7-i*7] = ROOK + i*6;
+				board[j*7][7-i*7] = new Rook(board,j*7,7-i*7,);
 				board[1+j*5][7-i*7] = KNIGHT + i*6;
 				board[2+j*3][7-i*7] = BISHOP + i*6;
 			}
@@ -78,168 +66,168 @@ public class ChessBoard { // pawn, knight, rook, bishop, king, queen //white, bl
 		return x >= 0 && x < row && y >= 0 && y < col;
 	}
 
-	public void setPiece(int x, int y, int id) {
+	public void setPiece(int x, int y, ChessPiece piece) {
 		if (!onBoard(x, y)) {
 			return;
 		}
-		board[x][y] = id;
+		board[x][y] = piece;
 		paint();
 	}
 
-	public int getPiece(int x, int y) {
+	public ChessPiece getPiece(int x, int y) {
 		if (!onBoard(x, y)) {
-			return -1;
+			return null;
 		}
 		return board[x][y];
 	}
 
-	private void checkCheck(){
-		sideChecked = -1;
-		for (int i = 0; i < row * col; i++) {
-			int kx = i % row;
-			int ky = i / row;
-			if (getPiece(kx,ky) % 6 == KING){
-				for (int j = 0; j < row * col; j++) {
-					int cx = j % row;
-					int cy = j / row;
-					if (getPiece(cx,cy) < 0) continue;
-					if (getPiece(cx,cy) / 6 != getPiece(kx,ky) / 6){
-						if (pieceTypes[getPiece(cx,cy) % 6].isMoveValid(cx,cy,kx-cx,ky-cy,getPiece(cx,cy) / 6)){
-							sideChecked = getPiece(kx,ky) / 6;
+//	private void checkCheck(){
+//		sideChecked = -1;
+//		for (int i = 0; i < getSize() * getSize(); i++) {
+//			int kx = i % getSize();
+//			int ky = i / getSize();
+//			if (getPiece(kx,ky) == KING){
+//				for (int j = 0; j < getSize() * getSize(); j++) {
+//					int cx = j % getSize();
+//					int cy = j / getSize();
+//					if (getPiece(cx,cy) < 0) continue;
+//					if (getPiece(cx,cy) / 6 != getPiece(kx,ky) / 6){
+//						if (pieceTypes[getPiece(cx,cy) % 6].isMoveValid(cx,cy,kx-cx,ky-cy,getPiece(cx,cy) / 6)){
+//							sideChecked = getPiece(kx,ky) / 6;
+//
+//						}
+//					}
+//				}
+//			}
+//		}
+//
+//	}
 
-						}
-					}
-				}
-			}
-		}
-
-	}
-
-	public int move(int x1, int y1, int x2, int y2) {
-		if (!onBoard(x1, y1) || !onBoard(x2, y2) || (x1 == x2 && y1 == y2)) {
-			return -1; //outta the board
-		}
-		int p1 = getPiece(x1, y1);
-		if (p1 == -1) {
-			return -1; //missing piece
-		}
-
-		int p2 = getPiece(x2, y2);
-		if (p2 > -1) {
-			if (p2 / 6 == p1 / 6) {//cant take ur own pieces
-				return -1;
-			}
-		}
-
-		if (!pieceTypes[p1 % 6].isMoveValid(x1, y1, x2 - x1, y2 - y1, p1 / 6))
-			return -1; //invalid move
-
-		if (p1 % 6 == PAWN && (y2 > y1 ? y2 - y1: y1 - y2) == 2){
-			enPassantFile = x1; //log pawn leap
-		}else{
-			enPassantFile = -1;
-		}
-
-
-
-
-
-		setPiece(x2, y2, p1);
-		setPiece(x1, y1, -1);
-		checkCheck();
-		if (sideChecked == nextSideToMove){ //check not putting self at check
-			setPiece(x1, y1, p1);
-			setPiece(x2, y2, p2);
-			return -1;
-		}
-
-		//did u take en passant
-		if (p1 % 6 == PAWN){
-			if (p2 == -1){
-				if (x1 != x2){
-					System.out.println("u used en passant");
-					p2 = getPiece(x2,y1);
-					setPiece(x2,y1,-1);
-				}
-			}
-		}
-
-		else if (p1 % 6 == KING){ //did u castle
-			if (x2 - x1 == 2 || x2 - x1 == -2){
-				setPiece(((x2 - x1) / 2 + 1 ) * 7,y1,-1);
-			}
-		}
-
-		if (p1 % 6 == KING){ //log if king or rook moved
-			if (p1 / 6 == 0){
-				whiteCastleDir = -2;
-			}
-			else{
-				blackCastleDir = -2;
-			}
-		} else if (p1 % 6 == ROOK) {
-			if (p1 / 6 == 0){
-				if (x1 == 0){
-					if (whiteCastleDir == 0) whiteCastleDir = 1;
-					else if (whiteCastleDir == -1) whiteCastleDir = -2;
-				}
-				else if (x1 == 7){
-					if (whiteCastleDir == 0) whiteCastleDir = -1;
-					else if (whiteCastleDir == 1) whiteCastleDir = -2;
-				}
-			}else{
-				if (x1 == 0){
-					if (blackCastleDir == 0) blackCastleDir = 1;
-					else if (blackCastleDir == -1) blackCastleDir = -2;
-				}
-				else if (x1 == 7){
-					if (blackCastleDir == 0) blackCastleDir = -1;
-					else if (blackCastleDir == 1) blackCastleDir = -2;
-				}
-			}
-		}
-		if (p1 % 6 == KING){
-			if ((x2 > x1 ? x2-x1 : x1 - x2) > 1){
-				System.out.println("castle?");
-			}
-		}
-
-		nextSideToMove = 1 - nextSideToMove;
-
-		if (p2 % 6 == KING){
-			//wow u killed the king, nice
-			sideWon = 1-(p2/6);
-			System.out.println(sideWon);
-		}
-		return p2; //piece that's been taken, -1 if empty
-	}
+//	public int move(int x1, int y1, int x2, int y2) {
+//		if (!onBoard(x1, y1) || !onBoard(x2, y2) || (x1 == x2 && y1 == y2)) {
+//			return -1; //outta the board
+//		}
+//		int p1 = getPiece(x1, y1);
+//		if (p1 == -1) {
+//			return -1; //missing piece
+//		}
+//
+//		int p2 = getPiece(x2, y2);
+//		if (p2 > -1) {
+//			if (p2 / 6 == p1 / 6) {//cant take ur own pieces
+//				return -1;
+//			}
+//		}
+//
+//		if (!pieceTypes[p1 % 6].isMoveValid(x1, y1, x2 - x1, y2 - y1, p1 / 6))
+//			return -1; //invalid move
+//
+//		if (p1 % 6 == PAWN && (y2 > y1 ? y2 - y1: y1 - y2) == 2){
+//			enPassantFile = x1; //log pawn leap
+//		}else{
+//			enPassantFile = -1;
+//		}
+//
+//
+//
+//
+//
+//		setPiece(x2, y2, p1);
+//		setPiece(x1, y1, -1);
+//		checkCheck();
+//		if (sideChecked == nextSideToMove){ //check not putting self at check
+//			setPiece(x1, y1, p1);
+//			setPiece(x2, y2, p2);
+//			return -1;
+//		}
+//
+//		//did u take en passant
+//		if (p1 % 6 == PAWN){
+//			if (p2 == -1){
+//				if (x1 != x2){
+//					System.out.println("u used en passant");
+//					p2 = getPiece(x2,y1);
+//					setPiece(x2,y1,-1);
+//				}
+//			}
+//		}
+//
+//		else if (p1 % 6 == KING){ //did u castle
+//			if (x2 - x1 == 2 || x2 - x1 == -2){
+//				setPiece(((x2 - x1) / 2 + 1 ) * 7,y1,-1);
+//			}
+//		}
+//
+//		if (p1 % 6 == KING){ //log if king or rook moved
+//			if (p1 / 6 == 0){
+//				whiteCastleDir = -2;
+//			}
+//			else{
+//				blackCastleDir = -2;
+//			}
+//		} else if (p1 % 6 == ROOK) {
+//			if (p1 / 6 == 0){
+//				if (x1 == 0){
+//					if (whiteCastleDir == 0) whiteCastleDir = 1;
+//					else if (whiteCastleDir == -1) whiteCastleDir = -2;
+//				}
+//				else if (x1 == 7){
+//					if (whiteCastleDir == 0) whiteCastleDir = -1;
+//					else if (whiteCastleDir == 1) whiteCastleDir = -2;
+//				}
+//			}else{
+//				if (x1 == 0){
+//					if (blackCastleDir == 0) blackCastleDir = 1;
+//					else if (blackCastleDir == -1) blackCastleDir = -2;
+//				}
+//				else if (x1 == 7){
+//					if (blackCastleDir == 0) blackCastleDir = -1;
+//					else if (blackCastleDir == 1) blackCastleDir = -2;
+//				}
+//			}
+//		}
+//		if (p1 % 6 == KING){
+//			if ((x2 > x1 ? x2-x1 : x1 - x2) > 1){
+//				System.out.println("castle?");
+//			}
+//		}
+//
+//		nextSideToMove = 1 - nextSideToMove;
+//
+//		if (p2 % 6 == KING){
+//			//wow u killed the king, nice
+//			sideWon = 1-(p2/6);
+//			System.out.println(sideWon);
+//		}
+//		return p2; //piece that's been taken, -1 if empty
+//	}
 
 	public int getSideWon(){
 		return sideWon;
 	}
 
-	public void selectPiece(Point boardPosition) {
-		int piece = getPiece(boardPosition.x, boardPosition.y);
-		if (piece < 0 || piece / 6 != nextSideToMove) return;
-		if (selected == null) {
-			selected = boardPosition;
-		}
-		paint();
-		main.boardPanel.enterDrag(getSelectedSprite());
-	}
+//	public void selectPiece(Point boardPosition) {
+//		int piece = getPiece(boardPosition.x, boardPosition.y);
+//		if (piece < 0 || piece / 6 != nextSideToMove) return;
+//		if (selected == null) {
+//			selected = boardPosition;
+//		}
+//		paint();
+//		main.boardPanel.enterDrag(getSelectedSprite());
+//	}
 
-	public void movePiece(Point boardPosition) {
-		if (selected == null) return;
-		move(selected.x, selected.y, boardPosition.x, boardPosition.y);
-		selected = null;
-		paint();
-		main.boardPanel.exitDrag();
-	}
+//	public void movePiece(Point boardPosition) {
+//		if (selected == null) return;
+//		move(selected.x, selected.y, boardPosition.x, boardPosition.y);
+//		selected = null;
+//		paint();
+//		main.boardPanel.exitDrag();
+//	}
 	
 	public BufferedImage getSelectedSprite() {
-		int piece = board[selected.x][selected.y];
+		ChessPiece piece = board[selected.x][selected.y];
 		BufferedImage sprite;
-		sprite = ResourceLoader.instance.getPiece(piece % 6, piece / 6);
+		sprite = ResourceLoader.instance.getPiece(piece .id, piece.side);
     	return sprite;
     }
 
