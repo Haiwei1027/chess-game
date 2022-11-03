06a7908325cdcbb2ce0807f33c07b14d0692ea80
Isaac Allport <48483898+Caasitroplla@users.noreply.github.com>
2022-11-03 13:40:52 +0000

    Added the ability for the piece its self to move

--- src/main/java/me/haiwei/pieces/ChessPiece.java
+++ src/main/java/me/haiwei/pieces/ChessPiece.java
@@ -19,4 +19,21 @@ public abstract class ChessPiece {
 
     public abstract boolean isMoveValid(int x, int y, int dx, int dy, int side);
 
+    public boolean movePiece(ChessPiece piece, int x, int y, int dx, int dy, int side){
+        // Check peice is not taking its own side
+        if (board.getPiece((x + dx), (y + dy)).getSide() == side) return false;
+
+        // Check if move is valid
+        if (!(piece.isMoveValid(x, y, dx, dy, side))) return false;
+
+        // Do move two cases taking piece and not
+        board.setPiece(x + dx, y + dy, piece);
+        board.setPiece(x, y, null);
+        return true;
+    }
+
+    public int getSide(){
+        return white ? 0 : 1;
+    }
+
 }
