6a140aff8aac8b83a2898103c95949ef3a4b30b9
Isaac Allport <48483898+Caasitroplla@users.noreply.github.com>
2022-11-03 14:11:03 +0000

    Bishop move check

--- src/main/java/me/haiwei/pieces/Bishop.java
+++ src/main/java/me/haiwei/pieces/Bishop.java
@@ -10,18 +10,37 @@ public class Bishop extends ChessPiece{
 	}
 
 	@Override
-	public boolean isMoveValid(int x, int y, int dx, int dy, int side) {
+	public boolean isMoveValid(int to_x, int to_y, int from_x, int from_y) {
+		// Make sure that the bishop is in positions from
+		if (board.getPiece(from_x, from_y).getId() != id) return false;
+
+		// Find the change in x and y
+		int dx = to_x - from_x;
+		int dy = to_y - from_y;
+
+		// Make sure moving in diagonal line only
 		if (!(dx == dy || dx == -dy)) return false;
-		int i = x,j = y;
-		while (i != x + dx){
-			if (i != x){
-				if (board.getPiece(i,j) > -1){
+
+		// Make sure that the bishop is not moving through any pieces
+		int i = from_x, j = from_y;
+		while (i != to_x || j != to_y){
+			if (i != from_x || j != from_y){
+				if (board.getPiece(i,j).getId() > -1){
 					return false;
 				}
 			}
-			i += dx > 0 ? 1 : -1;
-			j += dy > 0 ? 1 : -1;
+			if (dx == 0){
+				j += dy > 0 ? 1 : -1;
+			} else if (dy == 0){
+				i += dx > 0 ? 1 : -1;
+			} else {
+				i += dx > 0 ? 1 : -1;
+				j += dy > 0 ? 1 : -1;
+			}
 		}
+
+		// Make sure that the bishop is not moving to a position with a piece of the same color
+		if (board.getPiece(to_x, to_y).isWhite() == this.isWhite()) return false;
 		return true;
 	}
 
