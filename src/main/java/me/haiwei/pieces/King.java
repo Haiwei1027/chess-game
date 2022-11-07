a934e9308b5020d4c09bd852b8f17e1f1b917d0b
Isaac Allport <48483898+Caasitroplla@users.noreply.github.com>
2022-11-07 15:18:48 +0000

    King Basic Movement No castling implemented in isMoveValid

--- src/main/java/me/haiwei/pieces/King.java
+++ src/main/java/me/haiwei/pieces/King.java
@@ -11,10 +11,25 @@ public class King extends ChessPiece{
 		this.id = board.KING;
 	}
 
+	// Doesn't account for castling
 	@Override
-	public boolean isMoveValid(int x, int y, int dx, int dy, int side) {
-		return false;
+	public boolean isMoveValid(int to_x, int to_y, int from_x, int from_y) {
+		// Make sure that the king is in positions from
+		if (board.getPiece(from_x, from_y).getId() != id) return false;
 
+		// Find the change in x and y
+		int dx = to_x - from_x;
+		int dy = to_y - from_y;
+
+		// Make sure that the king is moving only one square
+		if (Math.abs(dx) > 1 || Math.abs(dy) > 1) return false;
+
+		// Make sure that the king is not moving to a position with a piece of the same color
+		if (board.getPiece(to_x, to_y).isWhite() == this.isWhite()) return false;
+
+		return true;
 	}
+
+
 
 }
